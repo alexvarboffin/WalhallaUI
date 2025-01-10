060df49e8f81c404fa4d61e975856c8307d1e27c
combo <alexvarboffin@gmail.com>
2025-01-10 21:45:30 +0300

    casino

--- wvcompose/build.gradle.kts
+++ wvcompose/build.gradle.kts
@@ -1,59 +1,32 @@
+extra.apply {
+    set("minSdkVersion", 21)
+    set("okHttpVersion", "4.12.0")
+    set("ONESIGNAL_APP_ID", "")
+    set("APPSFLYER_DEV_KEY", "")
+    set("FACEBOOK_SDK", "")
+    set("MYTRACKER_KEY", "")
+    set("APP_METRICA", "")
+}
+
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
+    alias(libs.plugins.kotlinAndroid) apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
-android {
-    namespace = "com.walhalla.wvcompose"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.walhalla.wvcompose"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+        jcenter()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
-dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
+
+fun versionCodeDate(): Int {
+    return java.text.SimpleDateFormat("yyMMdd").format(java.util.Date()).toInt()
+} 
